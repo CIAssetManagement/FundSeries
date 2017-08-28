6a8075d234290eb87e8925139f82eef0dac8b648
Mauricio <MauricioTrejo@users.noreply.github.com>
2017-08-28 14:00:56 -0500

    Removing "'"
    Forgot to remove ' symbol

--- fondos.R
+++ fondos.R
@@ -1,7 +1,7 @@
 #Clasificación de los clientes en serie accionaria.
 serie <- function(monto){
   #Series that investors can achieve
-  series <- c("'C-0","'C-1","'C-2","'C-3","'C-4")
+  series <- c("C-0","C-1","C-2","C-3","C-4")
   #Minimum money required per series
   montos <- c(100000000,30000000,5000000,1000000,10000)
   
@@ -10,5 +10,5 @@ serie <- function(monto){
   if(between(monto,montos[3],montos[2])){return(series[3])}
   if(between(monto,montos[4],montos[3])){return(series[4])}
   if(between(monto,montos[5],montos[4])){return(series[5])}
-  #if(monto < montos[5]){series[5]}
+  if(monto < montos[5]){return(series[5])}
 }
