eaea326575f67828821446bad7fb9e560ce9f41b
Mauricio <MauricioTrejo@users.noreply.github.com>
2017-10-31 09:48:25 -0600

    Nueva función

--- GeneradorOrdenes.R
+++ GeneradorOrdenes.R
@@ -12,6 +12,18 @@ setwd("C:/Github/FundSeries")
 archivo <- read_excel("OrdenPrueba.xls")
 archivo$Serie <- gsub("'","",archivo$Serie)
 
+diainhabil <-  function(fecha){
+  fechabase0 <- as.Date("2017-08-06")
+  fechabase1 <- as.Date("2017-08-07")
+  if(as.integer(fecha - fechabase0 ) %% 7 == 6){
+    fecha = fecha + 2 
+  }
+  if(as.integer(fecha - fechabase1 ) %% 7 == 6){
+    fecha = fecha + 1 
+  }
+  return(fecha)
+}
+
 ####################################################################################################
 #                                      Fondos a Reclasificar                                       #
 ####################################################################################################
